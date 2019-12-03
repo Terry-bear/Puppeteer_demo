5bde13889be07cbbdc38d58b6ee58c0383d354ff
Terry-zh <496971418@qq.com>
2019-12-03 15:08:02 +0800

    [update]

--- crawler.js
+++ crawler.js
@@ -1,9 +1,13 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+const gm = require("gm");
+
 const { readFiles } = require("./readF");
+
 const B_WIDTH = 1440;
 const B_HEIGHT = 880;
 const IdArr = readFiles("sinan");
+console.log(IdArr);
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -13,8 +17,8 @@ const IdArr = readFiles("sinan");
     deviceScaleFactor: 1
   });
   console.log(`浏览器窗口设定成功!: ${B_WIDTH}*${B_HEIGHT}`);
-  await page.goto("http://118.31.123.155/user/login");
-  
+  page.on('load', () => console.log('Page loaded!', page.url()));
+  await page.goto("http://47.99.128.84:10001/user/login");
   // TODO 登录的逻辑
   await page.focus("#userName");
   await page.keyboard.sendCharacter("admin");
@@ -23,15 +27,33 @@ const IdArr = readFiles("sinan");
   const login_btn = await page.$("button");
   await login_btn.click();
   console.log("登录成功 !");
-  await delay(2000);
-  
-  // TODO 进入详情页面的逻辑
-  await page.goto(`http://118.31.123.155/orderflow/detail?id=44428`);
-  await delay(5000);
-  const Locus_btn = await page.$("button.ant-btn-primary");
-  await Locus_btn.click();
-  await page.waitFor(5000);
-  await page.screenshot({ path: "example.png" });
+  await delay(2000)
+  // TODO 创建📂文件夹
+  fs.mkdir('./dist', { recursive: true }, (err) => {
+    if (err) console.log('没有此文件夹,自动创建dist');
+  });
+  for (const obj of IdArr) {
+    // TODO 进入详情页面的逻辑
+    await page.goto(`http://47.99.128.84:10001/orderflow/detail?id=${obj.ID}`);
+    await delay(5000);
+    fs.mkdirSync('dist/' + obj.ID);
+    await page.screenshot({
+      path: './dist/' + obj.ID + "/detail.png",
+      fullPage: true,
+    });
+    // gm('./dist/' + obj.ID + "/detail.png").chop(1215, 1200, 225);
+    const Locus_btn = await page.$("button.ant-btn-primary");
+    await Locus_btn.click();
+    await page.waitFor(5000);
+    console.log('come in the trajectory !');
+    await page.screenshot({
+      path: "./dist/" + obj.ID + "/flowDetail.png",
+      fullPage: true,
+    });
+    console.log('wancheng ..')
+  }
+  // 生成 'screen' media 格式的pdf.
+  // await page.screenshot({ path: "example.png" });
 
   
   await browser.close();
